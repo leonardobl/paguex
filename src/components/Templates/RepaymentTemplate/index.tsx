c448650230c04a1b34fa5e181b8940fdb047affb
Leonardo Lima <leonardo@paguextech.com>
2024-06-20 12:58:23 -0300

    criacao da tabela de reembolso

--- src/components/Templates/RepaymentTemplate/index.tsx
+++ src/components/Templates/RepaymentTemplate/index.tsx
@@ -8,6 +8,49 @@ export const RepaymentTemplate = () => {
     <LayoutTemplate title="Dashboard de Reembolsos">
       <S.Container>
         <FormFilterRepayment submitForm={(e) => console.log(e)} />
+
+        <S.Table>
+          <S.TableHead>
+            <tr>
+              <td>Empresa</td>
+              <td>Loja</td>
+              <td>Qtd. Total</td>
+              <td>Valor Total</td>
+            </tr>
+          </S.TableHead>
+          <S.TableBody>
+            <tr>
+              <td>Starcheck</td>
+              <td>São Luís</td>
+              <td>80</td>
+              <td>R$00,00</td>
+            </tr>
+            <tr>
+              <td>Starcheck</td>
+              <td>São Luís</td>
+              <td>80</td>
+              <td>R$00,00</td>
+            </tr>
+            <tr>
+              <td>Starcheck</td>
+              <td>São Luís</td>
+              <td>80</td>
+              <td>R$00,00</td>
+            </tr>
+            <tr>
+              <td>Starcheck</td>
+              <td>São Luís</td>
+              <td>80</td>
+              <td>R$00,00</td>
+            </tr>
+          </S.TableBody>
+          <S.TableFooter>
+            <tr>
+              <td colSpan={3}></td>
+              <td>Geral: R$00,00 </td>
+            </tr>
+          </S.TableFooter>
+        </S.Table>
       </S.Container>
     </LayoutTemplate>
   );
--- src/components/Templates/RepaymentTemplate/styles.ts
+++ src/components/Templates/RepaymentTemplate/styles.ts
@@ -1,3 +1,57 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 35rem;
+
+  &,
+  thead,
+  thead td,
+  tbody td {
+    border: 0.5px solid #cbcbcb;
+  }
+`;
+export const TableHead = styled.thead`
+  background: #1d1d1b;
+  height: 3.625rem;
+
+  td {
+    color: #fff;
+    text-align: center;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.25rem;
+    min-width: 11rem;
+  }
+`;
+export const TableBody = styled.tbody`
+  background: #f6f6f6;
+
+  td {
+    color: #a3a3a3;
+    text-align: center;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.25rem;
+    height: 3.625rem;
+  }
+`;
+
+export const TableFooter = styled.tfoot`
+  height: 1.875rem;
+  background: #111;
+
+  td {
+    color: #fff;
+    text-align: center;
+    font-size: 0.75rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.25rem;
+  }
+`;
