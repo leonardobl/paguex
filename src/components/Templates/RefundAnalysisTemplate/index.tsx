d9d9b648360977f568c96a101a2aa4073ffb0750
Leonardo Lima <leonardo@paguextech.com>
2024-09-17 17:06:30 -0300

    criacao layout relatorio analitico reembolso

--- src/components/Templates/RefundAnalysisTemplate/index.tsx
+++ src/components/Templates/RefundAnalysisTemplate/index.tsx
@@ -14,6 +14,44 @@ export const RefundAnalysisTemplate = () => {
           <img alt="icon download" src="/assets/svg/icon-export-down.svg" />{" "}
           Exportar
         </S.ButtonExport>
+
+        <S.Table>
+          <S.TableHeader>
+            <h3>Empresa</h3>
+            <h3>Unidade</h3>
+            <h3>Tipo de Atendimento</h3>
+            <h3>Tipo deserviço</h3>
+            <h3>Placa/Chassi</h3>
+            <h3>Valor</h3>
+          </S.TableHeader>
+
+          <S.TableItem>
+            <p>StarCheck</p>
+            <p>São Luís</p>
+            <p>Loja</p>
+            <p>Transferência</p>
+            <p>xxxxxxxxxxxxxxxxxx</p>
+            <p>R$00,00</p>
+          </S.TableItem>
+
+          <S.TableItem>
+            <p>StarCheck</p>
+            <p>São Luís</p>
+            <p>Itinerante</p>
+            <p>1° Emplacamento</p>
+            <p>xxxxxxxxxxxxxxxxxx</p>
+            <p>R$00,00</p>
+          </S.TableItem>
+
+          <S.TableItem>
+            <p>StarCheck</p>
+            <p>Timon</p>
+            <p>Loja</p>
+            <p>Transferência</p>
+            <p>xxxxxxxxxxxxxxxxxx</p>
+            <p>R$00,00</p>
+          </S.TableItem>
+        </S.Table>
       </S.Container>
     </LayoutTemplate>
   );
--- src/components/Templates/RefundAnalysisTemplate/styles.ts
+++ src/components/Templates/RefundAnalysisTemplate/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 72.4375rem;
+`;
 
 export const WrapperFilter = styled.div`
   margin-bottom: 3rem;
@@ -13,4 +16,54 @@ export const ButtonExport = styled.button`
   display: flex;
   gap: 0 8px;
   align-items: center;
+`;
+
+export const Table = styled.div`
+  margin-bottom: 2rem;
+
+  > div + div {
+    margin-top: 1.25rem;
+  }
+`;
+
+export const TableHeader = styled.div`
+  border-radius: 0.25rem;
+  background: #1d1d1b;
+  box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 0.2);
+  display: grid;
+  grid-template-columns: repeat(5, 1fr) 0.5fr;
+  align-items: center;
+  padding: 4px 1rem;
+  height: 3.5625rem;
+
+  h3 {
+    color: #fff;
+    font-family: "Noto Sans";
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
+    letter-spacing: 0.03rem;
+  }
+`;
+
+export const TableItem = styled.div`
+  border-radius: 0.25rem;
+  background: #f6f6f6;
+  box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 0.2);
+  align-items: center;
+  padding: 4px 1rem;
+  display: grid;
+  height: 3.5625rem;
+  grid-template-columns: repeat(5, 1fr) 0.5fr;
+
+  p {
+    color: #a3a3a3;
+    font-family: "Noto Sans";
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.5rem;
+    letter-spacing: 0.03rem;
+  }
 `;
